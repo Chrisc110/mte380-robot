a93e16fbf138152f3d258faffbea7c73dafb7843
Christopher Chung <tps90067@gmail.com>
2023-09-26 16:05:20 -0400

    WIP: add function prototypes and skeleton

--- Core/Inc/tcs3200_colour.h
+++ Core/Inc/tcs3200_colour.h
@@ -5,28 +5,61 @@
 #include "stdint.h"
 
 typedef struct{
-    GPIO_TypeDef* s0Port;   //Output freq scaling selection bit 0
-    uint16_t      s0Pin;
-
-    GPIO_TypeDef* s1Port;   //Output freq scaling selection bit 1
-    uint16_t      s0Pin;
-
-    GPIO_TypeDef* s2Port;   //Photodiode type selection bit 0
-    uint16_t      s0Pin;
-
-    GPIO_TypeDef* s3Port;   //Photodiode type selection bit 1
-    uint16_t      s0Pin;
-
-    TIM_HandleTypeDef* tim;
-
-    tsc3200_int_data_t intData;
-
-} tcs3200_colour_t;
-
-typedef struct{
     uint16_t t1;
     uint16_t t2;
     uint16_t isFirst;
-} tsc3200_int_data_t;
+} tcs3200_int_data_t;
+
+typedef struct{
+    GPIO_TypeDef* s0Port;       //Output freq scaling selection bit 0
+    uint16_t      s0Pin;
+
+    GPIO_TypeDef* s1Port;       //Output freq scaling selection bit 1
+    uint16_t      s1Pin;
+
+    GPIO_TypeDef* s2Port;       //Photodiode type selection bit 0
+    uint16_t      s2Pin;
+
+    GPIO_TypeDef* s3Port;       //Photodiode type selection bit 1
+    uint16_t      s3Pin;
+
+    TIM_HandleTypeDef* tim;     //Timer used to measure the square wave frequency
+
+    tcs3200_int_data_t intData; //Data type to hold the interrupt timer data, USER DO NOT MODIFY
+
+} tcs3200_colour_handle_t;
+
+typedef enum{
+    RED = 0,
+    GREEN,
+    BLUE,
+    CLEAR
+} tcs3200_colour_e;
+
+/// @brief do some initialization
+/// @param colSensor colour sensor handle
+void tcs3200_init(tcs3200_colour_handle_t colSensor);
+
+/// @brief enable the timer input capture interrupt
+/// @param colSensor colour sensor handle
+void tsc3200_start(tcs3200_colour_handle_t colSensor);
+
+/// @brief disable the timer input capture interrupt
+/// @param colSensor colour sensor handle
+void tsc3200_stop(tcs3200_colour_handle_t colSensor);
+
+/// @brief sets the s2 and s3 control bits to set the colour to read
+/// @param colSensor colour sensor handle
+/// @param colour desired colour to read
+void tcs3200_select_colour(tcs3200_colour_handle_t colSensor, tcs3200_colour_e colour);
+
+/// @brief reads the most recent colour reading
+/// @param colSensor colour sensor handle
+/// @return frequency of the square wave, in Hz
+uint16_t tcs3200_read(tcs3200_colour_handle_t colSensor);
+
+/// @brief implementation of the interrupt handler
+/// @param colSensor colour sensor handle
+void tcs3200_interrupt_handler(tcs3200_colour_handle_t colSensor);
 
 #endif /* TCS3200_COLOUR_H_ */
--- Core/Src/tcs3200_colour.c
+++ Core/Src/tcs3200_colour.c
@@ -1,1 +1,35 @@
 #include "tcs3200_colour.h"
+
+
+void tcs3200_init(tcs3200_colour_handle_t colSensor)
+{
+
+}
+
+
+void tsc3200_start(tcs3200_colour_handle_t colSensor)
+{
+
+}
+
+
+void tsc3200_stop(tcs3200_colour_handle_t colSensor)
+{
+
+}
+
+void tcs3200_select_colour(tcs3200_colour_handle_t colSensor, tcs3200_colour_e colour)
+{
+
+}
+
+uint16_t tcs3200_read(tcs3200_colour_handle_t colSensor)
+{
+    uint16_t val;
+    return val;
+}
+
+void tcs3200_interrupt_handler(tcs3200_colour_handle_t colSensor)
+{
+    
+}
