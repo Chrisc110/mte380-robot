bebff62490f3e20ce7ce949a4b47855debdaffb7
Christopher Chung <tps90067@gmail.com>
2023-09-26 17:27:25 -0400

    WIP: colour sensor function implementation

--- Core/Inc/tcs3200_colour.h
+++ Core/Inc/tcs3200_colour.h
@@ -8,6 +8,7 @@ typedef struct{
     uint16_t t1;
     uint16_t t2;
     uint16_t isFirst;
+    uint16_t freq;
 } tcs3200_int_data_t;
 
 typedef struct{
@@ -24,6 +25,7 @@ typedef struct{
     uint16_t      s3Pin;
 
     TIM_HandleTypeDef* tim;     //Timer used to measure the square wave frequency
+    uint32_t timChannel;        //Timer channel
 
     tcs3200_int_data_t intData; //Data type to hold the interrupt timer data, USER DO NOT MODIFY
 
@@ -36,30 +38,42 @@ typedef enum{
     CLEAR
 } tcs3200_colour_e;
 
+typedef enum{
+    TCS3200_NO_ERROR = 0,
+    TCS3200_ERROR,
+    TCS3200_INVALID_PARAMETER
+} tcs3200_status_e;
+
 /// @brief do some initialization
 /// @param colSensor colour sensor handle
-void tcs3200_init(tcs3200_colour_handle_t colSensor);
+/// @return tcs3200 status code
+tcs3200_status_e tcs3200_init(tcs3200_colour_handle_t* colSensor);
 
 /// @brief enable the timer input capture interrupt
 /// @param colSensor colour sensor handle
-void tsc3200_start(tcs3200_colour_handle_t colSensor);
+/// @return tcs3200 status code
+tcs3200_status_e tsc3200_start(tcs3200_colour_handle_t* colSensor);
 
 /// @brief disable the timer input capture interrupt
 /// @param colSensor colour sensor handle
-void tsc3200_stop(tcs3200_colour_handle_t colSensor);
+/// @return tcs3200 status code
+tcs3200_status_e tsc3200_stop(tcs3200_colour_handle_t* colSensor);
 
 /// @brief sets the s2 and s3 control bits to set the colour to read
 /// @param colSensor colour sensor handle
 /// @param colour desired colour to read
-void tcs3200_select_colour(tcs3200_colour_handle_t colSensor, tcs3200_colour_e colour);
+/// @return tcs3200 status code
+tcs3200_status_e tcs3200_select_colour(tcs3200_colour_handle_t* colSensor, tcs3200_colour_e colour);
 
 /// @brief reads the most recent colour reading
 /// @param colSensor colour sensor handle
-/// @return frequency of the square wave, in Hz
-uint16_t tcs3200_read(tcs3200_colour_handle_t colSensor);
+/// @return frequency of the square wave, in Hz, through "freq"
+/// @return tcs3200 status code
+tcs3200_status_e tcs3200_read(tcs3200_colour_handle_t* colSensor, uint16_t* freq);
 
 /// @brief implementation of the interrupt handler
 /// @param colSensor colour sensor handle
-void tcs3200_interrupt_handler(tcs3200_colour_handle_t colSensor);
+/// @return tcs3200 status code
+tcs3200_status_e tcs3200_interrupt_handler(tcs3200_colour_handle_t* colSensor);
 
 #endif /* TCS3200_COLOUR_H_ */
--- Core/Src/tcs3200_colour.c
+++ Core/Src/tcs3200_colour.c
@@ -1,35 +1,116 @@
 #include "tcs3200_colour.h"
 
+/* Helper Function Definitions */
+static tcs3200_status_e tcs3200_set_red(tcs3200_colour_handle_t* colSensor);
+static tcs3200_status_e tcs3200_set_green(tcs3200_colour_handle_t* colSensor);
+static tcs3200_status_e tcs3200_set_blue(tcs3200_colour_handle_t* colSensor);
+static tcs3200_status_e tcs3200_set_clear(tcs3200_colour_handle_t* colSensor);
 
-void tcs3200_init(tcs3200_colour_handle_t colSensor)
+/********************************/
+
+static tcs3200_status_e tcs3200_set_red(tcs3200_colour_handle_t* colSensor)
 {
+    HAL_GPIO_WritePin(colSensor->s2Port, colSensor->s2Pin, 0);
+    HAL_GPIO_WritePin(colSensor->s3Port, colSensor->s3Pin, 0);
 
+    return TCS3200_NO_ERROR;
+}
+
+static tcs3200_status_e tcs3200_set_green(tcs3200_colour_handle_t* colSensor)
+{
+    HAL_GPIO_WritePin(colSensor->s2Port, colSensor->s2Pin, 1);
+    HAL_GPIO_WritePin(colSensor->s3Port, colSensor->s3Pin, 1);
+
+    return TCS3200_NO_ERROR;
+}
+
+static tcs3200_status_e tcs3200_set_blue(tcs3200_colour_handle_t* colSensor)
+{
+    HAL_GPIO_WritePin(colSensor->s2Port, colSensor->s2Pin, 0);
+    HAL_GPIO_WritePin(colSensor->s3Port, colSensor->s3Pin, 1);
+
+    return TCS3200_NO_ERROR;
+}
+
+static tcs3200_status_e tcs3200_set_clear(tcs3200_colour_handle_t* colSensor)
+{
+    HAL_GPIO_WritePin(colSensor->s2Port, colSensor->s2Pin, 1);
+    HAL_GPIO_WritePin(colSensor->s3Port, colSensor->s3Pin, 0);
+
+    return TCS3200_NO_ERROR;
 }
 
 
-void tsc3200_start(tcs3200_colour_handle_t colSensor)
+tcs3200_status_e tcs3200_init(tcs3200_colour_handle_t* colSensor)
 {
-
+    //cube ide should have already initalized all the gpios, timers, and interrupts
+    return TCS3200_NO_ERROR;
 }
 
 
-void tsc3200_stop(tcs3200_colour_handle_t colSensor)
+tcs3200_status_e tsc3200_start(tcs3200_colour_handle_t* colSensor)
 {
+    //check if the timer channel input capture interrupt is already enabled?
+    //enable the timer channel input capture interrupt
+    return TCS3200_NO_ERROR;
+}
+
+
+tcs3200_status_e tsc3200_stop(tcs3200_colour_handle_t* colSensor)
+{
+    //check if the timer channel input capture interrupt is already disabled?
+    //disable the timer channel input capture interrupt
+    return TCS3200_NO_ERROR;
 
 }
 
-void tcs3200_select_colour(tcs3200_colour_handle_t colSensor, tcs3200_colour_e colour)
+tcs3200_status_e tcs3200_select_colour(tcs3200_colour_handle_t* colSensor, tcs3200_colour_e colour)
 {
+    tcs3200_status_e status = TCS3200_NO_ERROR;
 
+    switch (colour)
+    {
+        case RED:
+            tcs3200_set_red(colSensor);
+            break;
+
+        case GREEN:
+            tcs3200_set_green(colSensor);
+            break;
+
+        case BLUE:
+            tcs3200_set_blue(colSensor);
+            break;
+
+        case CLEAR:
+            tcs3200_set_clear(colSensor);
+            break;
+
+        default:
+            status = TCS3200_INVALID_PARAMETER;
+    }
+
+    return status;
 }
 
-uint16_t tcs3200_read(tcs3200_colour_handle_t colSensor)
+tcs3200_status_e tcs3200_read(tcs3200_colour_handle_t* colSensor, uint16_t* freq)
 {
-    uint16_t val;
-    return val;
+    *freq = colSensor->intData.freq;
+    return TCS3200_NO_ERROR;
 }
 
-void tcs3200_interrupt_handler(tcs3200_colour_handle_t colSensor)
+tcs3200_status_e tcs3200_interrupt_handler(tcs3200_colour_handle_t* colSensor)
 {
-    
+    //first rising edge
+    if (colSensor->intData.isFirst)
+    {
+        colSensor->intData.t1 = colSensor->tim->Instance->CCR1;
+    }
+    else
+    {
+
+    } 
+
+
+    return TCS3200_NO_ERROR;
 }
