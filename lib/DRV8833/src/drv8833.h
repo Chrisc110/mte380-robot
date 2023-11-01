6c3da7af7fc6b40ab9437f15b2038fdedea9d994
Christopher Chung <tps90067@gmail.com>
2023-10-31 22:52:35 -0400

    add colour sensor driver and motor driver

---
+++ lib/DRV8833/src/drv8833.h
@@ -0,0 +1,36 @@
+#ifndef DRV8833_H
+#define DRV8833_H
+
+#include <Arduino.h>
+#include <stdint.h>
+
+
+#define PWM_CONTROL_FREQ 50000
+#define MAX_DUTY_CYCLE 255
+
+typedef enum{
+    DRV8833_FORWARD = 0,
+    DRV8833_REVERSE
+} drv8833_dir_e;
+
+class DRV8833
+{
+    public:
+    DRV8833(uint32_t in1, uint32_t in2);
+    ~DRV8833();
+
+    void drive(drv8833_dir_e dir, float speedPercent);
+
+    void stop();
+
+    private:
+    uint32_t _in1;
+    uint32_t _in2;
+    
+    void drive_forward(float speedPercent);
+    void drive_reverse(float speedPercent);
+
+
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
+#include <ISL29125.h>
+#include <drv8833.h>
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+
 
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
+  delay(50);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);   
-  }
+  delay(50);
+  Serial.print("Hello\n");
+  } 
