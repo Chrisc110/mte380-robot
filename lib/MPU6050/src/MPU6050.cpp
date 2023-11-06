b9a6e04720578cb5fd4c9c4c5546ba62c274a98e
Ria Narang <rianarang6@gmail.com>
2023-11-05 23:56:06 -0500

    upload non working changes

--- lib/MPU6050/src/MPU6050.cpp
+++ lib/MPU6050/src/MPU6050.cpp
@@ -2,8 +2,8 @@
 #include "Arduino.h"
 #include "Wire.h"
 
-MPU6050::MPU6050(){}
-MPU6050::~MPU6050(){}
+MPU6050::MPU6050() {}
+MPU6050::~MPU6050() {}
 
 void MPU6050::init()
 {
@@ -15,21 +15,22 @@ void MPU6050::init()
     Wire.write(0x00);
     Wire.endTransmission();
 
-    for (uint32_t i = 0; i < 2000; i++) {
+    for (uint32_t i = 0; i < 2000; i++)
+    {
         MPU6050Data data;
         getAll(&data);
-        gyroCalibrationX+=data.gyroX;
-        gyroCalibrationY+=data.gyroY;
-        gyroCalibrationZ+=data.accZ;
+        gyroCalibrationX += data.gyroX;
+        gyroCalibrationY += data.gyroY;
+        gyroCalibrationZ += data.accZ;
         delay(1);
     }
 
-    gyroCalibrationX/=2000;
-    gyroCalibrationY/=2000;
-    gyroCalibrationZ/=2000;
+    gyroCalibrationX /= 2000;
+    gyroCalibrationY /= 2000;
+    gyroCalibrationZ /= 2000;
 }
 
-void MPU6050::getAll(MPU6050Data* data)
+void MPU6050::getAll(MPU6050Data *data)
 {
     Wire.beginTransmission(0x68);
     Wire.write(0x1A);
@@ -41,28 +42,28 @@ void MPU6050::getAll(MPU6050Data* data)
     Wire.endTransmission();
     Wire.beginTransmission(0x68);
     Wire.write(0x3B);
-    Wire.endTransmission(); 
-    Wire.requestFrom(0x68,6);
+    Wire.endTransmission();
+    Wire.requestFrom(0x68, 6);
     int16_t AccXLSB = Wire.read() << 8 | Wire.read();
     int16_t AccYLSB = Wire.read() << 8 | Wire.read();
     int16_t AccZLSB = Wire.read() << 8 | Wire.read();
 
     Wire.beginTransmission(0x68);
-    Wire.write(0x1B); 
+    Wire.write(0x1B);
     Wire.write(0x8);
-    Wire.endTransmission();     
+    Wire.endTransmission();
     Wire.beginTransmission(0x68);
     Wire.write(0x43);
     Wire.endTransmission();
-    Wire.requestFrom(0x68,6);
-    int16_t GyroX=Wire.read()<<8 | Wire.read();
-    int16_t GyroY=Wire.read()<<8 | Wire.read();
-    int16_t GyroZ=Wire.read()<<8 | Wire.read();
+    Wire.requestFrom(0x68, 6);
+    int16_t GyroX = Wire.read() << 8 | Wire.read();
+    int16_t GyroY = Wire.read() << 8 | Wire.read();
+    int16_t GyroZ = Wire.read() << 8 | Wire.read();
 
-    data->gyroX = (float)GyroX/65.5 - accCalibrationX;
-    data->gyroY = (float)GyroY/65.5 - accCalibrationY;
-    data->gyroZ = (float)GyroZ/65.5 - accCalibrationZ;
-    data->accX = (float)AccXLSB/4096;
-    data->accY = (float)AccYLSB/4096;
-    data->accZ = (float)AccZLSB/4096;
+    data->gyroX = (float)GyroX / 65.5 - accCalibrationX;
+    data->gyroY = (float)GyroY / 65.5 - accCalibrationY;
+    data->gyroZ = (float)GyroZ / 65.5 - accCalibrationZ;
+    data->accX = (float)AccXLSB / 4096;
+    data->accY = (float)AccYLSB / 4096;
+    data->accZ = (float)AccZLSB / 4096;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ Jordan McConnell @ SparkFun Electronics
 11 Apr 2014
 https://github.com/sparkfun/SparkFun_ISL29125_Breakout_Arduino_Library
 
-This example declares an SFE_ISL29125 object called RGB_sensor. The 
+This example declares an SFE_ISL29125 object called RGB_sensor. The
 object/sensor is initialized with a basic configuration so that it continuously
 samples the light intensity of red, green and blue spectrums. These values are
 read from the sensor every 2 seconds and printed to the Serial monitor.
@@ -18,7 +18,7 @@ Requires:
 SparkFun_ISL29125_Arduino_Library
 
 This code is beerware.
-Distributed as-is; no warranty is given. 
+Distributed as-is; no warranty is given.
 ******************************************************************************/
 
 #include <Wire.h>
@@ -27,6 +27,9 @@ Distributed as-is; no warranty is given.
 #include <MPU6050.h>
 #include "defines.h"
 #include "helpers.h"
+#include "math.h"
+
+#define YAW_SCALING_FACTOR 26.54
 
 // Instantiate colour sensors
 TwoWire colSenWire1(COL_SEN_SDA_1, COL_SEN_SCL_1);
@@ -39,10 +42,58 @@ DRV8833 motor1(MOTOR1_IN1, MOTOR1_IN2);
 DRV8833 motor2(MOTOR2_IN1, MOTOR2_IN2);
 
 // Instantiate IMU
-MPU6050 imu();
+MPU6050 imu;
 
 bool isStop = true;
 
+void turn(float degrees, float speed1, float speed2)
+{
+  MPU6050Data data;
+  float theta = 0;
+  uint32_t lastReadTime = 0;
+  drv8833_dir_e dir1 = degrees > 0 ? DRV8833_FORWARD : DRV8833_REVERSE;
+  drv8833_dir_e dir2 = degrees > 0 ? DRV8833_REVERSE : DRV8833_FORWARD;
+  motor1.drive(dir1, speed1);
+  motor2.drive(dir2, speed2);
+
+  while (abs(theta) * YAW_SCALING_FACTOR < abs(degrees))
+  {
+    imu.getAll(&data);
+    theta = (theta + data.gyroZ * 100.0f / 1000000.0f);
+    if (millis() - lastReadTime > 100)
+    {
+      Serial.print("theta ");
+      Serial.println(abs(theta * YAW_SCALING_FACTOR));
+
+      lastReadTime = millis();
+    }
+    delayMicroseconds(100);
+  }
+
+  // motor1.stop();
+  // motor2.stop();
+}
+
+void demoSequence()
+{
+  // 1 - 78, 2- 80
+  // rampDrive(motor1, DRV8833_FORWARD, 0.0f, 68.0f, motor2, DRV8833_FORWARD, 0.0f, 70.0f);
+  // delay(2000);
+
+  // detect red
+
+  // Do 360 turn
+  turn(90.0f, 68.0f, 70.0f);
+  motor1.stop();
+  motor2.stop();
+
+  // drive backwards
+  // rampDrive(motor1, DRV8833_REVERSE, 0.0f, 68.0f, motor2, DRV8833_REVERSE, 0.0f, 70.0f);
+}
+
+void check_red()
+{
+}
 
 void setup()
 {
@@ -71,9 +122,10 @@ void setup()
     Serial.println("Colour Sensor 2 Initialization: FAILED");
   }
 
+  imu.init();
+
   Serial.println("Motor 1 Initialization: SUCCESSFUL");
   Serial.println("Motor 2 Initialization: SUCCESSFUL");
-
 }
 
 // Read sensor values for each color and print them to serial monitor
@@ -82,25 +134,23 @@ void loop()
 
   if (digitalRead(USER_BTN) == 0)
   {
-    delay(25);
+    delay(100);
     if (digitalRead(USER_BTN) == 0)
     {
-      if(isStop == true)
+      if (isStop == true)
       {
         isStop = false;
         delay(1000);
-        rampDrive(motor1, DRV8833_FORWARD, 0.0f, 78.0f, motor2, DRV8833_FORWARD, 0.0f, 80.0f);
+        demoSequence();
       }
       else
       {
-        isStop = true;
-        motor1.stop();
-        motor2.stop();
-        delay(750);
+        // isStop = true;
+        // motor1.stop();
+        // motor2.stop();
       }
     }
   }
 
   digitalToggle(LED_BUILTIN);
-
 }
